e692f9233ddf85562aaf3af144f2008dabcb77ac
ZiadAS40 <zead4036@gmail.com>
2024-08-06 00:36:02 +0300

    make api

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -74,15 +74,17 @@ class DBStorage:
     def close(self):
         """call remove() method on the private session attribute"""
         self.__session.remove()
-    
+
     def get(self, cls, id):
-        """Returns the object based on the class and its ID, or None if not found"""
+        """Returns the object based on the
+        class and its ID, or None if not found
+        """
         objs_dict = self.all()
         myobj = cls.__name__ + "." + id
         if myobj in objs_dict:
             return objs_dict[myobj]
         return None
-    
+
     def count(self, cls=None):
         """
         Returns the number of objects in storage matching the given class.
