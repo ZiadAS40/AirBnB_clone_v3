5cab9f91bbe4a64c63153b9cdb85ce2652c1fd95
ZiadAS40 <zead4036@gmail.com>
2024-08-26 13:52:43 +0300

    fix circular import

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """make the Blueprint"""
 
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 
 
 @app_views.route('/status', strict_slashes=False)
